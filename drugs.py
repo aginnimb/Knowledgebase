7cf936f2b35e7224ccd7b29ef2af5b104de2b4f0
aginni <36461685+aginnimb@users.noreply.github.com>
2019-05-23 11:47:08 -0400

    Update drugs.py
    changed the script to enhance the performance and writing a few records that are at a higher level.

--- drugs.py
+++ drugs.py
@@ -2,86 +2,64 @@ import sys
 import csv
 import xml.etree.cElementTree as ET
 
-# def parsedrug(drug):
-# 	drugbank_id = drug.find('nsstring:drugbank-id',ns)     #Extract data at level#1
-# 	#print(drugbank_id[1].text)    
-# 	description= drug.find('nsstring:description',ns)
-# 	try:
-# 	    v_description = description.text
-# 	except:
-# 	    v_description = 'unknown'
-# 	print(description)
-# 	cas_number= drug.find('nsstring:cas-number',ns)
-# 	try:
-# 	    v_cas_number = cas_number.text
-# 	except:
-# 	    v_cas_number = 'unknown'
+def parsedrugbank(drugbank, writer):
+	# print("I am here")
+	for drug in drugbank.findall('nsstring:drug',ns):
+		drugbank_id = drug.find('nsstring:drugbank-id',ns)     #Extract data at level#1
+		#print(drugbank_id[1].text)    
+		description= drug.find('nsstring:description',ns)
+		try:
+		    v_description = description.text
+		except:
+		    v_description = 'unknown'
+			#print(description)
+		cas_number= drug.find('nsstring:cas-number',ns)
+		try:
+		    v_cas_number = cas_number.text
+		except:
+		    v_cas_number = 'unknown'
 
-# 	for general_references in drug.findall('nsstring:general-references',ns):
-# 		for articles in general-references.findall('nsstring:articles',ns):
-# 			for article in articles.findall('nsstring:article',ns):
-# 				pubmed_id = article.find('nsstring:pubmed-id',ns)
-# 				try:
-# 					v_pubmed_id = pubmed_id.text
-# 				except:
-# 					v_pubmed_id = 'unknown'
-# 	return[drugbank_id[1].text,description.text,v_cas_number,v_pubmed_id] 
+		for general_references in drug.findall('nsstring:general-references',ns):
+			for articles in general_references.findall('nsstring:articles',ns):
+				for article in articles.findall('nsstring:article',ns):
+					pubmed_id = article.find('nsstring:pubmed-id',ns)
+					try:
+						v_pubmed_id = pubmed_id.text
+					except:
+						v_pubmed_id = 'unknown'
 
+		for external_identifiers in drug.findall('nsstring:external-identifiers',ns):
+			for external_identifier in external_identifiers.findall('nsstring:external-identifier',ns):
+				resource = external_identifier.find('nsstring:resource',ns)
+				try:
+					v_resource = resource.text
+				except:
+					v_resource = 'unknown'
+				identifier = external_identifier.find('nsstring:identifier',ns)
+				try:
+					v_identifier = identifier.text
+				except:
+					v_identifier = 'unknown'
+				writer.writerow([drugbank_id.text,v_description,v_cas_number,v_pubmed_id,v_resource,v_identifier]) #write the file to get all th external identifiers
 
-	# for categories in drug.findall('nsstring:categories',ns):
-	# 	for category in categories.findall('nsstring:category',ns):
-	#         mesh_id= category.find('nsstring:mesh-id',ns)
-	#         try:
-	#             v_mesh_id = mesh_id.text
-	#         except:
-	#             v_mesh_id = 'unknown'
-	# for drug-interactions in drug.findall('nsstring:drug-interactions',ns):
-	# 	for drug-interaction in drug.findall('nsstring:drug-interaction',ns):
-	# 		drugbank_id= drug.find('nsstring:drugbank-id',ns)
-	#         try:
-	#             v_drugbank_id = drugbank_id.text
-	#         except:
-	#             v_drugbank_id = 'unknown'
-	#         description= drug.find('nsstring:description',ns)
-	#         try:
-	#             v_description =  description.text
-	#         except:
-	#             v_description = 'unknown'
+		            
+ns ={'nsstring':'http://www.drugbank.ca'}
 
-	# for external-identifiers in drug.findall('nsstring:external-identifiers',ns):
-	# 	for external-identifier in drug.findall('nsstring:external-identifier',ns):
-	# 		drugproduct_id = external-identifier.find('nsstring:identifier')
-	            
-    		# return[drugbank_id.text,description.text,v_cas_number,v_pubmed_id] 
-			#return[drugbank_id.text,description.text,v_cas_number,v_pubmed_id] 
-
-# ns ={'nsstring':'http://www.hmdb.ca'}
-
-# drugheader=['description','cas_number','pubmed_id']
+drugheader=['drugbank_id','description','cas_number','pubmed_id','resource','identifier','smpdv_id',]
    
-# count=0
-# with open('drugstest.csv','w') as drugcsv:
-#     writer = csv.writer(drugcsv)
-#     writer.writerow(drugheader)
-
-# for event, elem in ET.iterparse('drugstest.xml'):
-#     #print(event, elem.tag)
-#     if event == 'end':
-#         if elem.tag == '{http://www.drugbank.ca}drug':
-#             row = parsedrug(elem)
-#             writer.writerow(row)
-#             elem.clear()
-#             count+=1
-#             sys.stderr.write("\r %d" %(count))
-
-# drugcsv.close()
-
+count=0
+drugcsv = open('drugstest.csv','w')
+writer = csv.writer(drugcsv)
+writer.writerow(drugheader)
 
 for event, elem in ET.iterparse('drugstest.xml'):
-    print(event, elem.tag)
     if event == 'end':
-        if elem.tag == '{http://www.hmdb.ca}drug':
-            print(elem)
-        elem.clear()
+        if elem.tag == '{http://www.drugbank.ca}drugbank':
+            row = parsedrugbank(elem, writer)
+            # writer.writerow(row)
+            elem.clear()
+            count+=1
+            sys.stderr.write("\r %d" %(count))
 
-#v_mesh_id,v_drugbank_id,drugproduct_id.text
+drugcsv.close()
+
