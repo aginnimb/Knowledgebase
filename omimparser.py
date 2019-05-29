dc6bdd9e4916d8a32e1638fbada0c07d7ef6b814
aginni <36461685+aginnimb@users.noreply.github.com>
2019-05-29 16:34:22 -0400

    Update omimparser.py
    Still in Progress....

--- omimparser.py
+++ omimparser.py
@@ -7,12 +7,6 @@
 # url = 'https://data.omim.org/downloads/1r6-54cxR_inA4Zf_bzd0w/genemap2.txt'
 # urllib.request.urlretrieve(url,'/Users/aginni/Documents/omim/omimgenemap2.txt')
 
-
-# def omimdata(self):
-# 	# header = Mim Number	Preferred Title; symbol	Alternative Title(s); symbol(s)	Included Title(s); symbols
-# 	data = (self.file, delimiter = '\t',header = None)
-# 	return data
-
 # omimdata= []
 def omimparser(self):
 	with open(file,'r') as mim:
@@ -38,6 +32,29 @@ def omimparser(self):
 				# print(data)
 
 
+omimdata= []	
+		
+def omimparser(self):
+	self.omimkey = ["Mim Number","Preferred Title","symbol","Alternative Title(s)","symbol(s)","Included Title(s)","symbols"]
+	self.omimvals = {key:None for key in self.omimkey}
+
+	with open(file,'r') as mim:
+		for line in mim.readlines().split(','):
+			print(line)
+			for i,colnames in enumerate(self.omimkey):
+				omimvals[omimkey] = omimkey[i]
+					# if "Preferred Title; symbol" in line:
+					# 	self.omimvals["Preferred_title"]= line.split(";")[0]
+					# 	self.omimvals["symbol"] = line.split(";")[1]
+			# elif "Alternative Title(s); symbol(s)" in line:
+			# 	self.omimvals["Alternative_titles"] = line.split(";")[0]
+			# 	self.omimvals["Alt_symbols"] = line.split(";")[1]
+			# elif "Included Title(s); symbols" in line:
+			# 	self.omimvals["Included_Titles"] = line.split(";")[0]
+			# 	self.omimvals["Included_symbols"] = line.split(";")[1]
+				# print(self.omimkey,self.omimvals)
+				# omimdata.append([min_number + '\t' + Preferred_title + '\t' + symbol+ '\t' +Alternative_titles+ '\t' +Alt_symbols+ '\t' +Included_Titles+ '\t' +Included_symbols])
+				# print(omimdata)
 
 
 file = ('/Users/aginni/Documents/omim/omimTitles.txt')
